9f967d29bb3aa0e4e10694d6da76905ec85108a0
Michael Rogers <michael@estatespace.com>
2022-10-21 20:01:16 -0700

    added hello.html and more logic

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
-const btn = document.getElementById("clickme");
+const catBtn = document.getElementById("clickme");
+const renderHelloBtn = document.getElementById("renderHello");
+class App {
+  constructor(state) {
+    this.state = state;
+  }
+}
 
+const myApp = new App(null);
+console.log(myApp);
+
+function greeter() {
+  console.log("hello");
+}
 function handleRenderCatImage(e) {
   e.preventDefault();
   console.log("working");
+  myApp.state = "hello";
+  console.log(myApp);
   const catImg = document.querySelector(".lazy");
   catImg.setAttribute("src", "catimage.jpg");
 }
 
-btn.addEventListener("click", handleRenderCatImage);
+function render() {
+  if (myApp.state === "hello") {
+    window.location = "hello.html";
+  }
+}
+
+function renderHello(e) {
+  e.preventDefault();
+  myApp.state = "hello";
+  render();
+}
+
+catBtn.addEventListener("click", handleRenderCatImage);
+renderHelloBtn.addEventListener("click", renderHello);
